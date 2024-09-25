96da5e9d75a52a1f71b95c2fd493994975828dce
JOHAN AKIN ARAUJO DA SILVA RODRIGUES <johan_a_rodrigues@estudante.sesisenai.org.br>
2024-09-25 17:22:06 -0300

    mudança para react

---
+++ PetMate/src/pages/Landing.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Landing.css'
+function Landing() {
+  return (
+    <div>
+          <div class="container">
+
+<div class="navbar-conteiner">
+  <div class="imagem-nav">
+      <img class="nav-img" src="./public/images/logo-bege-Photoroom.png" alt=""/>
+  </div>
+  
+  <div class="botoes-nav">
+      <a>Home</a>
+      <a>Sobre nós</a>
+      <a>Contato</a>
+  </div>
+</div>
+
+<div class="base">
+  <div class="texto">
+      <h2>Dê um Lar, Ganhe um Amigo!</h2>
+      <p class="texto-p">Adotar um animal é oferecer uma segunda chance a um ser que precisa de amor. Eles trazem alegria, companheirismo e transformam sua vida. Faça a diferença hoje: adote e descubra o poder do amor incondicional!
+      </p>
+      <button>CADASTRE-SE!</button>
+  </div>
+  <div>
+      <img class="fundo" src="./public/images/pets2-removebg.png" alt=""/>
+  </div>
+</div>
+
+</div>
+    </div>
+  )
+}
+
+export default Landing
