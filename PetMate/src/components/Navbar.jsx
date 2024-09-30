78d5b839c1ccde4dd0b2224fa3cdaf5be1add690
JOHAN AKIN ARAUJO DA SILVA RODRIGUES <johan_a_rodrigues@estudante.sesisenai.org.br>
2024-09-30 17:26:54 -0300

    rework landing page

---
+++ PetMate/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div>
+      <div class="navbar-conteiner">
+        <img class="nav-img" src="/images/logo-bege-Photoroom.png" alt=""/>
+        
+        <div class="botoes-nav">
+            <a>Home</a>
+            <a>Sobre nós</a>
+            <a>Contato</a>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
