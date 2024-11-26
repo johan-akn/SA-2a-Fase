3a5f231b2485c4ccda42190dcf5ecff4cc61a37e
johan-akn <johan.akn17@gmail.com>
2024-11-26 15:07:08 -0300

    back

---
+++ PetMate/src/apiService.js
@@ -0,0 +1,63 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:3000', // URL do backend
+});
+
+
+
+//Users
+
+export const getUsuarios = async () => {
+    const response = await api.get('/usuarios');
+    return response.data;
+};
+
+export const getUsuarioById = async (id) => {
+    const response = await api.get(`/usuarios/${id}`);
+    return response.data;
+};
+
+export const addUsuario = async (usuario) => {
+    const response = await api.post('/usuarios', usuario);
+    return response.data;
+};
+
+export const updateUsuario = async (id, usuario) => {
+    const response = await api.put(`/usuarios/${id}`, usuario);
+    return response.data;
+};
+
+export const deleteUsuario = async (id) => {
+    const response = await api.delete(`/usuarios/${id}`);
+    return response.data;
+};
+
+
+
+//Pets
+
+export const getPets = async () => {
+    const response = await api.get('/pets');
+    return response.data;
+};
+
+export const getPetById = async (id) => {
+    const response = await api.get(`/pets/${id}`);
+    return response.data;
+};
+
+export const addPet = async (pet) => {
+    const response = await api.post('/pets', pet);
+    return response.data;
+};
+
+export const updatePet = async (id, pet) => {
+    const response = await api.put(`/pets/${id}`, pet);
+    return response.data;
+};
+
+export const deletePet = async (id) => {
+    const response = await api.delete(`/pets/${id}`);
+    return response.data;
+};
