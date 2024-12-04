3f38be82465bb3b403d17700b6319127d9ee1628
johan-akn <johan.akn17@gmail.com>
2024-12-04 17:20:32 -0300

    sobre nós

--- PetMate/src/pages/Contato.jsx
+++ PetMate/src/pages/Contato.jsx
@@ -10,11 +10,41 @@ function Contato() {
     <div>
         <Navbar />
       <div className="contato-container">
-           <h1>Contato</h1>
+
+           {/* <h1>Contato</h1>
            <button onClick={logarNav}>Logar</button>
            {logado && <p>você está logado</p>}
-           {logado == false && <p>você está deslogado</p>}
-           
+           {logado == false && <p>você está deslogado</p>} */}
+
+           <div className="banner-contato">
+              <img src="/images/banner_info.svg" alt="" />
+           </div>
+
+           <div className="meio-info-container">
+              <div className="infos-contato">
+
+                <div className="info-missao">
+                  <h2>Nossa missão</h2>
+                  <hr className="barra-info"/>
+                </div>
+
+                <div className="info-visao">
+                  <h2>Visão</h2>
+                  <hr className="barra-info"/>
+                </div>
+
+                <div className="info-valores">
+                  <h2>Valores</h2>
+                  <hr className="barra-info"/>
+                </div>
+
+              </div>
+
+              <div className="fale-conosco">
+                <h2>Fale conosco</h2>
+                <img src="/images/dog_oculos.svg" className="dog-oculos" />
+              </div>
+           </div>
         </div>
     </div>
   )
